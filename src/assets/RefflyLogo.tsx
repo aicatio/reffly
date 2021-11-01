72493c53731fdcdaa9f238ecc5b7dae441a3230a
Ali Hasan <alihasan.iubat@gmail.com>
2021-11-02 01:29:14 +0600

    Used static url for logo

--- src/assets/RefflyLogo.tsx
+++ src/assets/RefflyLogo.tsx
@@ -1,14 +1,14 @@
 import * as React from 'react';
 import Grid from '@mui/material/Grid';
 import { Typography } from '@mui/material';
-const logo = require('../static/images/logos/reffly-logo-v1.svg');
+
 
 export default function RefflyLogo() {
   return (
     <Grid item xs={12}>
       <Grid container mt={5} justifyContent="center">
         <img
-          src={logo}
+          src="/images/logos/reffly-logo-v1.svg"
           alt="Reffly Logo"
           style={{ width: '20%', minWidth: '200px' }}
         />
--- src/assets/RefflyPlusLogo.tsx
+++ src/assets/RefflyPlusLogo.tsx
@@ -1,13 +1,13 @@
 import * as React from 'react';
 import Grid from '@mui/material/Grid';
-const logoPlus = require('../static/images/logos/refflyplus-logo-v1.svg');
+
 
 export default function RefflyLogo() {
   return (
     <Grid item xs={12} className="Logo">
       <Grid container mt={5} justifyContent="left">
         <img
-          src={logoPlus}
+          src="/images/logos/refflyplus-logo-v1.svg"
           alt="Reffly Plus Logo"
           style={{ width: '20%', minWidth: '200px' }}
         />
